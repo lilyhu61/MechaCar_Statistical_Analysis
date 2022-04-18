41fde9a18e80c38afb77cb8499c81580fed747e3
lily <95242493+lilyhu61@users.noreply.github.com>
2022-04-17 20:11:13 -0600

    Add files via upload

---
+++ MechaCarChallenge.R
@@ -0,0 +1,29 @@
+library(dplyr)
+
+# Deliverable 1:Linear Regression to Predict MPG
+# Import and read in the MechaCar_mpg.csv file as a dataframe.
+mechacar_mpg_df <- read.csv(file='MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
+head(mechacar_mpg_df)
+# multiple linear regression
+lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = mechacar_mpg_df)
+# find the p-value and r-squared
+summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = mechacar_mpg_df))
+
+
+# Deliverable 2: Create Visualizations for the Trip Analysis 
+# read csv to dataframe
+suspension_coil_df <- read.csv(file='Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+head(suspension_coil_df)
+# create a summary dataframe
+total_summary <- suspension_coil_df %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+# create summaries for each lot
+lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+
+# Deliverable 3: T-tests on suspension coils
+t.test(suspension_coil_df$PSI, mu=1500)
+
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
+
